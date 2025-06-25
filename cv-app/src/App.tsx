e8aa04cdd6f8d04eff5db49354e2b676786eafe7
JohanNilsenS <johan.nilsen.stjernquist@iths.se>
2025-06-25 21:33:49 +0200

    update(App.tsx)

--- cv-app/src/App.tsx
+++ cv-app/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Wow lets see if this goes out onto the internet
       </p>
     </>
   )
